f4939953cd6947549c24032f867ea7d802b8f4d1
Truong Hoang Dung <truonghoangdung1987hp@gmail.com>
2021-11-01 10:16:56 +0700

    Fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  experimental: {
-    reactRoot: true,
-    concurrentFeatures: true,
-    serverComponents: true,
-  },
+    experimental: {
+        reactRoot: true,
+        concurrentFeatures: true,
+        serverComponents: true,
+    },
+    async rewrites() {
+        return [
+            { "source": "/pokemon/:id", "destination": "/demo/pokemon?articleId=:id", "locale": false }
+        ]
+    }
 }
